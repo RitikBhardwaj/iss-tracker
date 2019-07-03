5ebae22e350ec2005a9a432e9f4af6a4c73abad6
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2019-07-03 14:20:27 +0530

    add the $.ajax() method using the Jquery library and also increse the refresh rate of the the information to every 1.5 seconds

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -1,25 +1,31 @@
-
 const url = "https://api.wheretheiss.at/v1/satellites/25544";
-setInterval(getLatLng,3000);
-function getLatLng(){
-    $.getJSON(url,data =>{
-        $('#latitude').text(' '+data.latitude.toFixed(3)+' ');
-        $('#longitude').text(' ' + data.longitude.toFixed(3)+' ');
-        $('#altitude').text(' ' + data.altitude.toFixed(2) + ' ');
-        $("#velocity").text(" " + data.velocity.toFixed(3) + " ");
-    }
-)};
 
-function changeGiphy(index){
-    $.getJSON(
-      'https://api.giphy.com/v1/gifs/search?api_key=QNMAgj5xQCnIARW3YiQ24rmuVqQSWS1r&q=nasa+iss&limit=100&offset=0&rating=G&lang=en'
-    ,data=>{
-        console.log(data.data[0].images.original.url);
-        $('body').css('background-image',`url(${data.data[index].images.original.url})`);
-    });
+//update the information every 1.5 seconds
+setInterval(getLatLng, 1500);
+function getLatLng() {
+  $.getJSON(url, data => {
+    $("#latitude").text(" " + data.latitude.toFixed(3) + " ");
+    $("#longitude").text(" " + data.longitude.toFixed(3) + " ");
+    $("#altitude").text(" " + data.altitude.toFixed(2) + " ");
+    $("#velocity").text(" " + data.velocity.toFixed(3) + " ");
+  });
 }
 
-$('button#change-giphy').click(()=>{
-    var index = Math.floor(100*Math.random());
-    changeGiphy(index);
+function changeGiphy(index) {
+  //ajax call to the api using jquery ajax
+  $.ajax({
+    url:
+      "https://api.giphy.com/v1/gifs/search?api_key=QNMAgj5xQCnIARW3YiQ24rmuVqQSWS1r&q=nasa+iss&limit=100&offset=0&rating=G&lang=en"
+  }).done(data => {
+    console.log(data.data[0].images.original.url);
+    $("body").css(
+      "background-image",
+      `url(${data.data[index].images.original.url})`
+    );
+  });
+}
+
+$("button#change-giphy").click(() => {
+  var index = Math.floor(100 * Math.random());
+  changeGiphy(index);
 });
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 app.use(express.static("Public"));
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+  res.render("index");
+});
 
-app.get("/", function (req, res) {
-    res.render('index');
-})
+const port = 8000 || process.env.PORT;
 
-app.listen(process.env.PORT, function () {
-    console.log("[+]Server started!");
+app.listen(port, function() {
+  console.log(`[+]Server started at port ${port}`);
 });
