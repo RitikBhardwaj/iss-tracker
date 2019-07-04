476735b4127c1519b410474a70459fb63719203b
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2019-07-05 01:57:18 +0530

    changed process.env.port to process.env.PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res) {
   res.render("index");
 });
 
-const port = process.env.port; //process.env.PORT;
+const port = process.env.PORT; //process.env.PORT;
 
 app.listen(port, function() {
   console.log(`[+]Server started at port ${port}`);
